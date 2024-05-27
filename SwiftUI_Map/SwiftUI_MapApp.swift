dbd341529ee23ca1e348867cfbaf07bcc4dca817
DANIS <danis.preldzic@sqa-consulting.com>
2024-05-27 08:44:49 +0200

    initial commit

---
+++ SwiftUI_Map/SwiftUI_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapApp.swift
+//  SwiftUI_Map
+//
+//  Created by Danis Preldzic on 27. 5. 2024..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
